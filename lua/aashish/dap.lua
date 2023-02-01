ef2f225ae3c6704cf3d0220cc4caac1bfb3e69ce
Aashish Adhikari <aashish@pop-os.localdomain>
2023-02-01 23:56:32 +0545

    minimal working config for neovim

---
+++ lua/aashish/dap.lua
@@ -0,0 +1,79 @@
+local dap = require('dap')
+local ui = require("dapui")
+
+vim.fn.sign_define('DapBreakpoint',{ text ='🟥', texthl ='', linehl ='', numhl =''})
+vim.fn.sign_define('DapStopped',{ text ='▶️', texthl ='', linehl ='', numhl =''})
+
+ui.setup()
+
+--------------------------------------------------
+--                C++ Config                    --
+--------------------------------------------------
+dap.adapters.codelldb = {
+  type = 'server',
+  port = "${port}",
+  executable = {
+    -- CHANGE THIS to your path!
+    command = '/home/aashish/.local/share/nvim/mason/bin/codelldb',
+    args = {"--port", "${port}"},
+
+    -- On windows you may have to uncomment this:
+    -- detached = false,
+
+    -- Don't forget to put -g while compiling 
+  }
+}
+dap.configurations.cpp = {
+  {
+    name = "Launch file",
+    type = "codelldb",
+    request = "launch",
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+    cwd = '${workspaceFolder}',
+    stopOnEntry = false,
+    runInTerminal = true,
+  },
+}
+dap.configurations.c = dap.configurations.cpp
+
+--------------------------------------------------
+--                Python Config                 --
+--------------------------------------------------
+require('dap-python').setup('~/.virtualenvs/debugpy/bin/python3')
+
+--------------------------------------------------
+--                Keymaps                       --
+--------------------------------------------------
+
+-- Start debugging session
+vim.keymap.set("n", "<localleader>dr", function()
+  dap.continue()
+  ui.toggle({})
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-w>=", false, true, true), "n", false) -- Spaces buffers evenly
+end)
+
+-- Set breakpoints, get variable values, step into/out of functions, etc.
+vim.keymap.set("n", "<localleader>dl", require("dap.ui.widgets").hover)
+vim.keymap.set("n", "<localleader>dc", dap.continue)
+vim.keymap.set("n", "<localleader>db", dap.toggle_breakpoint)
+vim.keymap.set("n", "<localleader>dn", dap.step_over)
+vim.keymap.set("n", "<localleader>di", dap.step_into)
+vim.keymap.set("n", "<localleader>do", dap.step_out)
+vim.keymap.set("n", "<localleader>dC", function()
+  dap.clear_breakpoints()
+end)
+
+-- Close debugger and clear breakpoints
+vim.keymap.set("n", "<localleader>de", function()
+  dap.clear_breakpoints()
+  ui.toggle({})
+  dap.terminate()
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-w>=", false, true, true), "n", false)
+end)
+
+-- python keymaps
+vim.keymap.set("n", "<localleader>dpm", require("dap-python").test_method)
+vim.keymap.set("n", "<localleader>dpc", require("dap-python").test_class)
+vim.keymap.set("n", "<localleader>dps", require("dap-python").debug_selection)
---
+++ lua/aashish/plugins.lua
@@ -0,0 +1,129 @@
+local fn = vim.fn
+
+-- Automatically install packer
+local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+  PACKER_BOOTSTRAP = fn.system {
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  }
+  print "Installing packer close and reopen Neovim..."
+  vim.cmd [[packadd packer.nvim]]
+end
+
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd [[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]]
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  return
+end
+
+-- Have packer use a popup window
+packer.init {
+  display = {
+    open_fn = function()
+      return require("packer.util").float { border = "rounded" }
+    end,
+  },
+}
+
+-- Install your plugins here
+return packer.startup(function(use)
+
+  --  Package Manager, manages itself 
+  use "wbthomason/packer.nvim"
+  --  An implementation of the Popup API from vim in Neovim
+  use "nvim-lua/popup.nvim"
+  -- Useful lua functions used by lots of plugins
+  use "nvim-lua/plenary.nvim"
+  -- Gruvbox theme which I'm liking alot
+  use { "ellisonleao/gruvbox.nvim" }
+
+  -- cmp plugins
+  use "hrsh7th/nvim-cmp" -- The completion plugin
+  use "hrsh7th/cmp-buffer" -- buffer completions
+  use "hrsh7th/cmp-path" -- path completions
+  use "hrsh7th/cmp-cmdline" -- cmdline completions
+  use "saadparwaiz1/cmp_luasnip" -- snippet completions
+  use "hrsh7th/cmp-nvim-lsp" -- works well with lsp
+
+  -- snippets
+  use "L3MON4D3/LuaSnip" --snippet engine
+  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+
+  -- LSP Configuration & Plugins
+  use {
+    'neovim/nvim-lspconfig',
+    requires = {
+      -- Automatically install LSPs to stdpath for neovim
+      'williamboman/mason.nvim',
+      'williamboman/mason-lspconfig.nvim',
+      -- Useful status updates for LSP
+      'j-hui/fidget.nvim',
+      -- Additional lua configuration, makes nvim stuff amazing
+      'folke/neodev.nvim',
+    },
+  }
+
+  -- Treesitter to highlight, edit, and navigate code
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = function()
+      pcall(require('nvim-treesitter.install').update { with_sync = true })
+    end,
+  }
+  -- Additional text objects via treesitter
+  use {
+    'nvim-treesitter/nvim-treesitter-textobjects',
+    after = 'nvim-treesitter',
+  }
+
+  -- Fuzzy Finder (files, lsp, etc)
+  use { 'nvim-telescope/telescope.nvim', branch = '0.1.x', requires = { 'nvim-lua/plenary.nvim' } }
+  -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
+  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', cond = vim.fn.executable 'make' == 1 }
+  use { "nvim-telescope/telescope-file-browser.nvim" }
+
+  -- Git related plugins
+  use 'tpope/vim-fugitive'
+  use 'tpope/vim-rhubarb'
+  use 'lewis6991/gitsigns.nvim'
+
+  -- Debugging stuff in neovim
+  use 'mfussenegger/nvim-dap'
+  use 'jayp0521/mason-nvim-dap'
+  use 'rcarriga/nvim-dap-ui'
+  use 'theHamsta/nvim-dap-virtual-text'
+  use 'nvim-telescope/telescope-dap.nvim'
+  use 'mfussenegger/nvim-dap-python'
+
+  -- Fancier statusline
+  use 'nvim-lualine/lualine.nvim'
+  -- Add indentation guides even on blank lines
+  use 'lukas-reineke/indent-blankline.nvim'
+  -- "gc" to comment visual regions/lines
+  use 'numToStr/Comment.nvim'
+  -- Detect tabstop and shiftwidth automatically
+  use 'tpope/vim-sleuth'
+  -- floating terminal
+  use {'akinsho/toggleterm.nvim', tag = '*' }
+  -- autopairs brackets and stuff 
+  use { "windwp/nvim-autopairs", config = function() require("nvim-autopairs").setup {} end}
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if PACKER_BOOTSTRAP then
+    require("packer").sync()
+  end
+end)
